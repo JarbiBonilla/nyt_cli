ac6f11c5a7dcf73eb8d093c1f58bc4c34f3d5efb
Jarbi Bonilla <jarbibonilla@gmail.com>
2019-12-01 01:41:40 +0000

    created instances in my api class.

--- lib/nyt_cli/api.rb
+++ lib/nyt_cli/api.rb
@@ -5,6 +5,14 @@ class NytCli::API
     url = "https://api.nytimes.com/svc/movies/v2/reviews/search.json?critics-pick=Y&api-key=#{key}"
     response = HTTParty.get(url)
     response.parsed_response["results"].each do |movie|
+      title = movie["display_title"]
+      critic = movie["byline"]
+      summary = movie["summary_short"]
+      link = movie["link"]["url"]
+      opening_date = movie["opening_date"]
+      
+      
+      NytCli::Movie
       binding.pry
     end 
   end 
