acfbe333b0c1fd2079716c05a842238f46d20864
Jarbi Bonilla <jarbibonilla@gmail.com>
2019-12-01 02:21:27 +0000

    created the call method in my cli class.

--- lib/nyt_cli/cli.rb
+++ lib/nyt_cli/cli.rb
@@ -3,6 +3,7 @@ class NytCli::CLI
   def call 
     
     puts "\n Welcome movie fanatics! Would you like to see the latest New York Times critic's movie picks?"
+    puts "y/n"
     NytCli::API.fetch
   end 
 end
